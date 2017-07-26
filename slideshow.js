4599c50d2c11228b5be06c28829482733f35d056
calicoeats <30420334+calicoeats@users.noreply.github.com>
2017-07-26 15:14:22 -0700

    Update slideshow.js

--- slideshow.js
+++ slideshow.js
@@ -3,9 +3,7 @@
   $(function(){
     jQuery(document).ready(function() {
     $('#home').backstretch([
-       "images/home-bg-slideshow1.jpg", 
-       "images/home-bg-slideshow2.jpg",
-       "images/home-bg-slideshow3.jpg",
-       "images/home-bg-slideshow4.jpg"
+       "background1.jpg", 
+       "background2.jpg",
         ],  {duration: 2000, fade: 750});
 });
