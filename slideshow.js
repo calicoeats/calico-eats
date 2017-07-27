f68bd03b468ff0927a338e13319ccc1347786a54
calicoeats <30420334+calicoeats@users.noreply.github.com>
2017-07-27 10:18:49 -0700

    Update slideshow.js

--- slideshow.js
+++ slideshow.js
@@ -1,9 +1,17 @@
  // HOME BACKGROUND SLIDESHOW
  
-  $(function(){
-    jQuery(document).ready(function() {
-    $('#home').backstretch([
-       "background1.jpg", 
-       "background2.jpg",
-        ],  {duration: 2000, fade: 750});
-});
+ 
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1} 
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
